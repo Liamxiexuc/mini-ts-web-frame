67d3dfb30ba7e3fa1499f053435e48f898a69d8b
liam <liachenxiexu@gmail.com>
2021-08-27 08:40:14 +1000

    update collection view

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
-import { User } from "./models/User";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
 
-const collection = User.buildUserCollection();
-collection.on("change", () => {
-  console.log(collection);
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on("change", () => {
+  const root = document.getElementById("root");
+
+  if (root) {
+    new UserList(root, users).render();
+  }
 });
 
-collection.fetch();
+users.fetch();
+
+// const user = User.buildUser({ name: "Jim", age: 20 });
+
+// const root = document.querySelector("#root");
+
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   throw new Error("no root found");
+// }
